b5203d1057330251132cf79cccd36945aafe0d51
LyAhn <jaybentham@gmail.com>
2024-01-02 19:26:11 +0000

    Base upload

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SocialCreditBot
- Just For Memes Discord Bot that reads the conversation and gives you a positive or negative social credit score depending on your messages
+ Just For Memes Discord Bot that reads the conversation and gives you a positive or negative social credit score depending on your messages using TextBlob Sentiment Analysis. - Super basic, super WIP. Don't use, this is purely for a joke and for practice.
+
+ # Usage
+
+ 1: Create token.txt and enter your bot token on the first line save and exit.
+
+ 2: Create keywords.txt and fill this out line by line with the words you want the bot to look out for.
+
+ 3: Run the bot and invite it to your server.
+
+ The bot will now monitor your conversation and look out for if any of the keywords in the text file were used, it then checks the sentiment of the message. 
---
+++ bot.py
@@ -0,0 +1,85 @@
+import sqlite3
+import discord
+
+
+from textblob import TextBlob
+
+############################Test stuff#################################
+with open('token.txt') as f:
+    token = f.read()
+
+keywords = []
+with open('keywords.txt') as f:
+    keywords = f.readlines()
+    keywords = [x.strip() for x in keywords]
+
+
+
+######################################################################
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    watching = discord.Activity(type=discord.ActivityType.watching, name="you! 🇨🇳")
+    await client.change_presence(activity=watching)
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if any(keyword in message.content.lower() for keyword in keywords):
+        blob = TextBlob(message.content)
+        sentiment = blob.sentiment.polarity
+        
+        score = 0
+        
+        if sentiment > 0.5:
+            score = 25
+            await message.channel.send(f'Positive social credit score for {message.author.mention}! You have been given {score}. :red_envelope: :flag_cn:')
+        elif sentiment > 0:
+            score = 10
+            await message.channel.send(f'Positive social credit score for {message.author.mention}! You have been given {score}.')
+        elif sentiment < -0.5:
+            score = -25
+            await message.channel.send(f'Negative social credit score for {message.author.mention}! You have been deducted {score} points! ')
+        elif sentiment < 0:
+            score = -10
+            await message.channel.send(f'Negative social credit score for {message.author.mention}! You have been deducted {score} points! ')
+
+        conn = sqlite3.connect('scores.db')
+        c = conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS scores (user_id INTEGER PRIMARY KEY, score INTEGER)')
+        c.execute('SELECT score FROM scores WHERE user_id = ?', (message.author.id,))
+        result = c.fetchone()
+        if result is None:
+            c.execute('INSERT INTO scores (user_id, score) VALUES (?, ?)', (message.author.id, score))
+            result = (score,)
+        else:
+            c.execute('UPDATE scores SET score = ? WHERE user_id = ?', (result[0] + score, message.author.id))
+        conn.commit()
+        conn.close()
+
+    if message.content.startswith('?score'):
+        user = message.author
+        if len(message.mentions) > 0:
+            user = message.mentions[0]
+        conn = sqlite3.connect('scores.db')
+        c = conn.cursor()
+        c.execute('CREATE TABLE IF NOT EXISTS scores (user_id INTEGER PRIMARY KEY, score INTEGER)')
+        c.execute('SELECT score FROM scores WHERE user_id = ?', (user.id,))
+        result = c.fetchone()
+        if result is None:
+            await message.channel.send(f':flag_cn: {user.mention} has not received a social credit score yet. :flag_cn:')
+        else:
+            await message.channel.send(f'{user.mention} has a social credit score of {result[0]}. :flag_cn:')
+
+    if message.content.startswith('?help'):
+        embed = discord.Embed(title='Social Credit Score Bot Help', description='Here are the available commands for the Social Credit Score Bot:', color=0xFF0000)
+        embed.add_field(name='?score', value='Displays the social credit score of the user who sent the command. You can also mention another user to see their social credit score.', inline=False)
+        embed.add_field(name='?help', value='Displays this help message.', inline=False)
+        await message.channel.send(embed=embed)
+
+client.run(token)
